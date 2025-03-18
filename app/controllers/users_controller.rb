381f87d1435f5e6fe847f320adff9ebeceaa994c
Billy Berner <billy.berner@kellogg.northwestern.edu>
2025-03-18 16:52:46 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.password = BCrypt::Password.create(params[:password])
 
     if @user.save
       flash[:notice] = "Thanks for signing up. Now login."
       redirect_to login_path
     else
-      flash[:alert] = "Signup failed. Please try again."
-      render :new
+      flash[:alert] = "Signup failed: " + @user.errors.full_messages.join(", ")
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password)
+    params.permit(:username, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,8 @@
-require 'bcrypt'
-
 class User < ApplicationRecord
-  include BCrypt
-
-  # Encrypt password before saving
-  before_save :encrypt_password
+  has_secure_password  # Enables built-in password encryption with BCrypt
 
   # Validations
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
-
-  # Virtual password field (not stored in the database)
-  attr_accessor :password
-
-  def authenticate(plain_text_password)
-    BCrypt::Password.new(self.password_digest) == plain_text_password ? self : false
-  end
-
-  private
-
-  def encrypt_password
-    if password.present?
-      self.password_digest = BCrypt::Password.create(password)
-    end
-  end
 end
