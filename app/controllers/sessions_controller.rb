f38179f5fe6ac08c66dce9ac8bd4b8954b83c5ed
Billy Berner <billy.berner@kellogg.northwestern.edu>
2025-03-18 16:59:19 +0000

    changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,18 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-
-    if @user && BCrypt::Password.new(@user.password_digest) == params[:password]
+  
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      puts "DEBUG: session[:user_id] = #{session[:user_id]}" # Debugging
       flash[:notice] = "Logged in successfully!"
-      redirect_to root_path
+      redirect_to places_path
     else
+      puts "DEBUG: Authentication failed" # Debugging
       flash[:alert] = "Invalid email or password."
       render :new
     end
-  end
+  end  
 
   def destroy
     session[:user_id] = nil
